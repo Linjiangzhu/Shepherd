51a038ecbb117db94c93813a6b50e784283a2c84
Linjiangzhu <yuyuex97@gmail.com>
2019-11-13 02:56:12 -0800

    add openai gym to simplify the model

--- src/openai-gym-env/main.py
+++ src/openai-gym-env/main.py
@@ -1,7 +1,11 @@
 import gym
-env = gym.make('CartPole-v0')
+from SheepHerder import SheepHerderEnvironment
+
+env = SheepHerderEnvironment()
 env.reset()
-for _ in range(1000):
-    env.render()
-    env.step(env.action_space.sample()) # take a random action
+
+for _ in range(6000):
+	env.render()
+	env.step(env.action_space.sample())
+
 env.close()
